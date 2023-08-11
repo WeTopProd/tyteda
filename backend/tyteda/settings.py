4df962d81a2c9c4c0c8a6ad4a7bae857ea9c062a
roma <roma.saipov@bk.ru>
2023-08-11 13:27:31 +0300

    front

--- backend/tyteda/settings.py
+++ backend/tyteda/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-*mzje=yktkay8l632q8$h)xzn#0a&218(jz^&17d$w4o1tbl^&'
 
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = [
