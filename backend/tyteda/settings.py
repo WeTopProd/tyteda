a1b758e099799e02d783c59d2a5eeef2ae716004
Yaroslav Loshkarev <dfdfdf.dfdf.1985@mail.ru>
2023-12-06 22:58:52 +0300

    add image

--- backend/tyteda/settings.py
+++ backend/tyteda/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-*mzje=yktkay8l632q8$h)xzn#0a&218(jz^&17d$w4o1tbl'
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = [
@@ -73,7 +73,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tyteda.wsgi.application'
 
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
